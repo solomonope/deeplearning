cfce59d286fbfed6b90117d15f32e3d947bb04f0
Folorunsho Solomon Opeyemi <s.folorunsho@mytaxi.com>
2017-08-08 00:27:09 +0200

    update

--- deepnet.py
+++ deepnet.py
@@ -1,13 +1,13 @@
-import keras;
-import keras.datasets as DataSet;
+import keras
+import keras.datasets as DataSets;
 import keras.models as Models;
 import keras.layers as Layers;
 import keras.optimizers as Optimizers;
 import numpy as NP;
 
-NP.random.seed(42);
+NP.random.seed(42)
 
-(X_train, y_train), (X_test, y_test) = DataSet.mnist.load_data();
+(X_train, y_train), (X_test, y_test) = DataSets.mnist.load_data();
 
 X_train = X_train.reshape(60000, 784).astype("float32");
 X_test = X_test.reshape(10000, 784).astype("float32");
@@ -23,12 +23,18 @@ y_test = keras.utils.to_categorical(y_test, num_classes=n_classes);
 model = Models.Sequential();
 
 model.add(Layers.Dense((64), activation='relu', input_shape=(784,)));
+model.add(Layers.BatchNormalization())
+model.add(Layers.Dropout(0.5))
 model.add(Layers.Dense((64), activation='relu'));
+model.add(Layers.BatchNormalization())
+model.add(Layers.Dropout(0.5));
+model.add(Layers.Dense((64), activation='relu'));
+model.add(Layers.BatchNormalization())
+model.add(Layers.Dropout(0.5));
 model.add(Layers.Dense((10), activation="softmax"));
-
 
 model.summary();
 
-model.compile(loss="categorical_crossentropy", optimizer=Optimizers.SGD(lr=0.001), metrics=['accuracy'])
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
 
-model.fit(X_train, y_train, batch_size=128, epochs=200, verbose=1, validation_data=(X_test, y_test))
+model.fit(X_train, y_train, batch_size=128, epochs=100, verbose=1, validation_data=(X_test, y_test))
