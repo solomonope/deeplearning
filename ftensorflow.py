6e60a7199112cca1d70147dc799d68553f6cf0cd
Folorunsho Solomon Opeyemi <s.folorunsho@mytaxi.com>
2017-08-14 11:36:01 +0200

    updates

--- ftensorflow.py
+++ ftensorflow.py
@@ -1,19 +1,19 @@
-import tensorflow as TF;
+import tensorflow as tf;
 import numpy as NP;
 
-x1 = TF.placeholder(TF.float32);
-x2 = TF.placeholder(TF.float32);
+x1 = tf.placeholder(tf.float32);
+x2 = tf.placeholder(tf.float32);
 
 print(x1);
 
-sum_op = TF.add(x1, x2);
-product_op = TF.multiply(x1, x2);
+sum_op = tf.add(x1, x2);
+product_op = tf.multiply(x1, x2);
 
-with TF.Session() as session:
+with tf.Session() as session:
     sum_result = session.run(sum_op, feed_dict={x1: 3, x2: 0.5});
     print(sum_result)
     mult_result = session.run(product_op, feed_dict={x1: 2.0, x2: 0.5});
 
-with TF.Session() as session:
+with tf.Session() as session:
     sum_result = session.run(sum_op, feed_dict={x1: [2.0, 2.0, 2.0], x2: [0.5, 1.0, 2.0]});
     mult_result = session.run(product_op, feed_dict={x1: 2.0, x2: 0.5});
